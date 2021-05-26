3b4ca0278f188af1c45dad24079dabaaa5de9a92
RostenRossDev <costantini.nestor.m@gmail.com>
2021-05-26 12:17:19 -0300

    Topic 5 was created.

---
+++ resources/Nestor_Costantini/topic5/EggsShopping/src/main/java/com/globant/bootcamp/EggsShopping/EggsShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.globant.bootcamp.EggsShopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggsShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggsShoppingApplication.class, args);
+	}
+
+}
