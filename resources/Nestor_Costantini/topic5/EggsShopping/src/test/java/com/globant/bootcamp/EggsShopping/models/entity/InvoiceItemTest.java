fe245c55bc662d07eb22ae17a45cae1e7b1238b3
RostenRossDev <costantini.nestor.m@gmail.com>
2021-05-31 00:55:00 -0300

    InvoiceItemTest file was created.

---
+++ resources/Nestor_Costantini/topic5/EggsShopping/src/test/java/com/globant/bootcamp/EggsShopping/models/entity/InvoiceItemTest.java
@@ -0,0 +1,14 @@
+package com.globant.bootcamp.EggsShopping.models.entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class InvoiceItemTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
